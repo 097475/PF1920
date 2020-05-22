3d0ae7644a264379eb250a107a075b57b0816e23
Rocche <giacomorocchetti97@gmail.com>
2020-05-22 15:02:43 +0200

    Added history to bfs return

--- algorithms.lua
+++ algorithms.lua
@@ -8,18 +8,24 @@ function manhattan(x1, y1, x2, y2)
     return math.abs(x1 - x2) + math.abs(y1 - y2)
 end
 
+-- input: maze, entry point encoded, coordinates of exit point
+-- output: list of all states
 function bfs(maze, entry_point_encoded, exit_y, exit_x)
     local visited = {}
-    local q = Queue:new()
+    local paths_queue = Queue:new()
+    local directions_queue = Queue:new()
     local path
-    q:enqueue({entry_point_encoded})
+    local directions_path
+    paths_queue:enqueue({entry_point_encoded})
+    directions_queue:enqueue({{"",0}})
     entry_life, entry_x, entry_y = decode(entry_point_encoded)
     table.insert(visited, {entry_y, entry_x})
-    while not q:isEmpty() do
-        path = q:dequeue()
+    while not paths_queue:isEmpty() do
+        path = paths_queue:dequeue()
+        directions_path = directions_queue:dequeue()
         local last_cell = path[#path]
         local life, x, y = decode(last_cell)
-        if x == exit_x and y == exit_y then return path end
+        if x == exit_x and y == exit_y then return path, directions_path end
         
         local available_moves = move_encode(last_cell, maze)
         for move, direction_life_difference in pairs(available_moves) do
@@ -27,8 +33,11 @@ function bfs(maze, entry_point_encoded, exit_y, exit_x)
             if (not table.contains(visited, {move_y, move_x})) and move_life > 0 then
                 table.insert(visited, {move_y, move_x})
                 local new_path = table.copy(path)
+                local new_direction = table.copy(directions_path)
                 table.insert(new_path, move)
-                q:enqueue(new_path)
+                table.insert(new_direction, direction_life_difference)
+                paths_queue:enqueue(new_path)
+                directions_queue:enqueue(new_direction)
             end
         end
     end
@@ -36,8 +45,8 @@ function bfs(maze, entry_point_encoded, exit_y, exit_x)
 end
 
 start, maze = init_game_data("mazes/maze_1.txt")
-q = bfs(maze,initial_state(start),4,6)
+path, history = bfs(maze,initial_state(start),4,6)
 
-for _, v in ipairs(q) do
-    print(v)
+for _, d in ipairs(history) do
+    print(d[1] .. ", " .. d[2])
 end
